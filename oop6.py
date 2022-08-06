c6581720c8897688a0bd208def33ff1da465e5bd
Abdul Aouwal <aouwalitshikkha@gmail.com>
2022-08-06 20:36:12 +0600

    Added Vedio 7

--- oop6.py
+++ oop6.py
@@ -1,29 +1,28 @@
 class Product:
-    def __init__(self, name: str, price: float, quantity: int, discount=0):
+    discount = 10
+
+    def __init__(self, name: str, price: float, quantity: int):
         # Data validation
         assert price >= 0, 'Price must be greater than 0'
         assert quantity >= 0, 'Quantity must be greater than 0'
-        assert discount >= 0, 'Discount must be greater than 0'
 
         # Assign to objects
         self.name = name
         self.price = price
         self.quantity = quantity
-        self.discount = discount
 
     def total_price(self):
         return self.price * self.quantity
 
     def net_payable(self):
-        main_percentage = 1 - self.discount/100
+        main_percentage = 1 - Product.discount/100
         net_amount = self.price * self.quantity * main_percentage
         return net_amount
 
 
 items = Product('Mobile Phone', 8000, 150)
 item2 = Product('Laptop', 10000, 100)
-item3 = Product('Moneybag', 280, 15, 10 )
+item3 = Product('Moneybag', 280, 15)
 
-# print(items.price)
-# print(items.total_price())
-print(item3.net_payable())
+print(Product.discount) # Class Level Attribute
+print(items.net_payable())  # Instance Level Attribute
--- oop7.py
+++ oop7.py
@@ -1,15 +1,19 @@
 class Product:
-    def __init__(self, name: str, price: float, quantity: int, discount=0):
+    '''
+    These data will show the details of product class.
+    Here discount rate is defauls 10, it will take name as string,price as float etc
+    '''
+    discount = 10
+
+    def __init__(self, name: str, price: float, quantity: int):
         # Data validation
         assert price >= 0, 'Price must be greater than 0'
         assert quantity >= 0, 'Quantity must be greater than 0'
-        assert discount >= 0, 'Discount must be greater than 0'
 
         # Assign to objects
         self.name = name
         self.price = price
         self.quantity = quantity
-        self.discount = discount
 
     def total_price(self):
         return self.price * self.quantity
@@ -22,8 +26,11 @@ class Product:
 
 items = Product('Mobile Phone', 8000, 150)
 item2 = Product('Laptop', 10000, 100)
-item3 = Product('Moneybag', 280, 15, 10 )
+item3 = Product('Moneybag', 280, 15)
 
-# print(items.price)
-# print(items.total_price())
-print(item3.net_payable())
+# print(Product.__doc__) #class level
+print(items.__)
+
+# __init__
+# __doc__
+# __dict__
